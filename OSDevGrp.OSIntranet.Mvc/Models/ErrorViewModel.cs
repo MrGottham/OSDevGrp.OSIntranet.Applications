28e83eea12d12cd16cc189bc20e521c3267c22a1
Ole Sørensen <mrgottham@gmail.com>
2019-03-30 23:39:09 +0100

    Adding MVC project

---
+++ OSDevGrp.OSIntranet.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OSDevGrp.OSIntranet.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
