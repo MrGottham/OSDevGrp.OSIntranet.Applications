f0e0b5b1915512bc7d594dff81d5905a5ade5d0b
Ole Sørensen <mrgottham@gmail.com>
2023-01-14 12:38:59 +0100

    Adding generic category to support categories, types etc.

---
+++ OSDevGrp.OSIntranet.Domain.Interfaces/Core/IGenericCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace OSDevGrp.OSIntranet.Domain.Interfaces.Core
+{
+    public interface IGenericCategory : IAuditable, IDeletable
+    {
+        public int Number { get; }
+
+        public string Name { get; }
+    }
+}
---
+++ OSDevGrp.OSIntranet.Repositories/Models/Core/GenericCategoryModelBase.cs
@@ -0,0 +1,13 @@
+﻿using OSDevGrp.OSIntranet.Domain.Core;
+
+namespace OSDevGrp.OSIntranet.Repositories.Models.Core
+{
+    internal abstract class GenericCategoryModelBase : AuditableBase
+    {
+        public abstract int GenericCategoryIdentifier { get; }
+
+        public string Name { get; set; }
+
+        public virtual bool Deletable { get; set; }
+    }
+}
